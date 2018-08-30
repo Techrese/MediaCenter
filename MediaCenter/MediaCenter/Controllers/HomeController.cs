6df27bb14cd92a403870699bc98fc836946fab69
tim clinckemalie <tim.clinckemalie@hotmail.com>
2018-08-30 11:16:43 +0200

    last changes

--- MediaCenter/MediaCenter/Controllers/HomeController.cs
+++ MediaCenter/MediaCenter/Controllers/HomeController.cs
@@ -5,33 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MediaCenter.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace MediaCenter.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _configuration;
+        private readonly Locations _locations;
+
+        public HomeController(IConfiguration configuration, IOptions<Locations> options)
+        {
+            _configuration = configuration;
+            _locations = options.Value;
+        }
+       
         public IActionResult Index()
         {
+            
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Settings()
         {
-            ViewData["Message"] = "Your application description page.";
+
+            //  var array =_configuration.GetSection("Locations:Location").GetChildren()
+            //    .Select(configsection => configsection.Value);
+
+
+            var locations = new Locations();
+            _configuration.GetSection("Locations").Bind(locations);
+
+            var items = locations.location.AsEnumerable();
+            var items2 = _locations;
+
+
 
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
--- MediaCenter/MediaCenter/Startup.cs
+++ MediaCenter/MediaCenter/Startup.cs
@@ -2,26 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediaCenter.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
 
 namespace MediaCenter
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            // Set the new Configuration
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set;  }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(config =>
+                {
+                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+            services.AddOptions();
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            services.Configure<Locations>(Configuration.GetSection("Locations:Location"));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,12 +51,13 @@ namespace MediaCenter
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
@@ -45,6 +66,8 @@ namespace MediaCenter
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            
         }
     }
 }
